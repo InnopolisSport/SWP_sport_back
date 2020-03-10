7337c5be495b61bb87599e84be0f8a26ba9b88c7
WinnerOK <danman339@gmail.com>
2020-03-10 13:55:45 +0300

    Template added
    #SPORT-27 State To Verify

---
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -0,0 +1,48 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.api.utils.db import get_db
+from app.api.utils.security import get_current_user
+from app.core import config
+from app.core.jwt import create_access_token
+from app.core.security import get_password_hash
+from app.db_models.user import User as DBUser
+from app.models.token import Token
+from app.models.user import User
+
+router = APIRouter()
+
+
+@router.post("/login/access-token", response_model=Token, tags=["login"])
+def login_access_token(
+    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
+):
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = crud.user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    elif not crud.user.is_active(user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": create_access_token(
+            data={"user_id": user.id}, expires_delta=access_token_expires
+        ),
+        "token_type": "bearer",
+    }
+
+
+@router.post("/login/test-token", tags=["login"], response_model=User)
+def test_token(current_user: DBUser = Depends(get_current_user)):
+    """
+    Test access token
+    """
+    return current_user
---
+++ backend/app/app/models/msg.py
@@ -0,0 +1,5 @@
+# from .base import Base
+#
+#
+# class Msg(Base):
+#     msg: str
