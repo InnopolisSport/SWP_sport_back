b2ceae9392cb382439f305e052ad9f3eb23e1469
WinnerOK <danman339@gmail.com>
2020-03-14 14:43:06 +0300

    login withot swagger integration

--- backend/app/app/core/security.py
+++ backend/app/app/core/security.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 from base64 import urlsafe_b64encode
 from typing import Optional
 
-from app.core.config import OAUTH_APP_ID, BASE_URL
+from app.core.config import OAUTH_APP_ID, BASE_URL, OAUTH_SHARED_SECRET
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -15,13 +15,26 @@ def get_auth_url():
     pass
 
 
-def get_auth_params(state: Optional[str]) -> dict:
+def get_code_retrieve_params(state: Optional[dict]) -> dict:
     return {
-        'response_type': 'code',
-        'client_id': OAUTH_APP_ID,
-        'redirect_uri': BASE_URL + 'get_code',
-        'scope': 'user_impersonation openid profile email allatclaims',
-        'state': urlsafe_b64encode(str(state).encode())
+        "response_type": "code",
+        "client_id": OAUTH_APP_ID,
+        "redirect_uri": f"{BASE_URL}/get_code/get_code",
+        "scope": "user_impersonation openid profile email allatclaims",
+        "state": urlsafe_b64encode(str(state).encode())
+    }
+
+
+def get_token_retrieve_params(code: str) -> dict:
+    return {
+        "grant_type": "authorization_code",
+        "code": code,
+        "response_type": "code",
+
+        "client_id": OAUTH_APP_ID,
+        "client_secret": OAUTH_SHARED_SECRET,
+
+        "redirect_uri": f"{BASE_URL}/get_code/get_code"
     }
 
 
