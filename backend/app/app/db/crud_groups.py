a9ba7b3dd6ab4b95dc6f08409f8c8f9b9628b814
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-03-14 10:54:38 +0300

    Add doc strings for crud methods

--- backend/app/app/db/crud_groups.py
+++ backend/app/app/db/crud_groups.py
@@ -1,31 +1,63 @@
 from app.db import conn
 
 
-def create_sport(name: str):
+def create_sport(name):
+    """
+    Creates new sport type
+    @param name: str - sport type name
+    """
     cursor = conn.cursor()
     cursor.execute('INSERT INTO sport (name) VALUES (%s)', (name,))
     conn.commit()
 
 
+def get_sports():
+    """
+    Retrieves existing sport types
+    @return list of tuples (sport_id, name)
+    """
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, name FROM sport')
+    return cursor.fetchall()
+
+
 def delete_sport(sport_id):
+    """
+    Deletes existing sport type by its id
+    @param sport_id: int - sport id
+    """
     cursor = conn.cursor()
     cursor.execute('DELETE FROM sport WHERE id=%s', (sport_id,))
     conn.commit()
 
 
-def create_group(name: str, sport_id: int):
+def create_group(name, sport_id, trainer_id=None):
+    """
+    Creates new sport group
+    @param name: str - new sport group name
+    @param sport_id: int - chosen sport type id
+    @param trainer_id: int - assigned trainer id
+    """
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO "group" (name, sport_id) VALUES (%s, %s)', (name, sport_id))
+    cursor.execute('INSERT INTO "group" (name, sport_id, trainer_id) VALUES (%s, %s, %s)', (name, sport_id, trainer_id))
     conn.commit()
 
 
 def get_groups():
+    """
+    Retrieves existing sport group
+    @return list of tuples (group_id, group_name, sport_name, trainer_id)
+    """
     cursor = conn.cursor()
-    cursor.execute('SELECT g.id, g.name, s.name FROM "group" g JOIN sport s ON g.sport_id = s.id')
+    cursor.execute('SELECT g.id, g.name, s.name, g.trainer_id FROM "group" g JOIN sport s ON g.sport_id = s.id')
     return cursor.fetchall()
 
 
 def delete_group(group_id):
+    """
+    Deletes existing group by its id
+    @param group_id: int - group id
+    """
     cursor = conn.cursor()
     cursor.execute('DELETE FROM "group" WHERE id=%s', (group_id,))
     conn.commit()
--- backend/app/app/db/crud_users.py
+++ backend/app/app/db/crud_users.py
@@ -1,36 +1,68 @@
 from app.db.connection import conn
 
 
-# type 0 = student
-# type 1 = trainer
-# type 2 = student + trainer
-# type 3 = admin
 def create_user(first_name: str, last_name: str, email: str, user_type: int, course_id: str = None):
+    """
+    Creates new user of any type
+    @param first_name: str - first name of created user
+    @param last_name: str - last name of created user
+    @param email: str - email of created user, should be unique
+    @param user_type: str - type of created user,
+        type 0 = student
+        type 1 = trainer
+        type 2 = student + trainer
+        type 3 = admin
+    @param course_id: str - course identifier, needed only for students, e. g. 'B18'
+    """
     cursor = conn.cursor()
     cursor.execute('INSERT INTO "user" (first_name, last_name, email, type, course_id) VALUES (%s, %s, %s, %s, %s)',
                    (first_name, last_name, email, user_type, course_id))
     conn.commit()
 
 
-def get_students():
+def get_students(course_id=None):
+    """
+    Retrieves registered students
+    @param course_id: str - particular course identifier, leave empty for choosing all courses
+    @return list of tuples (user_id, first_name, last_name, email, course_id)
+    """
     cursor = conn.cursor()
-    cursor.execute('SELECT id, first_name, last_name, email, course_id FROM "user" WHERE type=0 OR type=2')
+    if course_id is None:
+        cursor.execute('SELECT id, first_name, last_name, email, course_id FROM "user" WHERE type=0 OR type=2')
+    else:
+        cursor.execute(
+            'SELECT id, first_name, last_name, email, course_id FROM "user" WHERE (type=0 OR type=2) AND course_id=%s',
+            (course_id, ))
     return cursor.fetchall()
 
 
 def get_trainers():
+    """
+    Retrieves registered trainers / club-leaders
+    @return list of tuples (user_id, first_name, last_name, email)
+    """
     cursor = conn.cursor()
     cursor.execute('SELECT id, first_name, last_name, email FROM "user" WHERE type=1 OR type=2')
     return cursor.fetchall()
 
 
 def get_user(user_id):
+    """
+    Retrieves registered user by its id
+    @param user_id: int - searched user id
+    @return user tuple (user_id, first_name, last_name, email, course_id)
+    """
     cursor = conn.cursor()
-    cursor.execute('SELECT first_name, last_name, email, course_id FROM "user" WHERE id=%s', (user_id,))
+    cursor.execute('SELECT id, first_name, last_name, email, course_id FROM "user" WHERE id=%s', (user_id,))
     return cursor.fetchone()
 
 
 def find_user_by_email(email):
+    """
+    Finds registered user by its email
+    @param email: str - searched user email
+    @return user tuple (user_id, first_name, last_name, email, course_id)
+    """
     cursor = conn.cursor()
     cursor.execute('SELECT id, first_name, last_name, email, course_id FROM "user" WHERE email=%s', (email,))
     return cursor.fetchone()
