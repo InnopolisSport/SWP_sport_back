aa9f683b945e2ac46f2d277a053fb7d5f59668be
WinnerOK <danman339@gmail.com>
2020-03-16 12:03:26 +0300

    tagged pages, output status on login page

--- backend/app/app/pages/pages.py
+++ backend/app/app/pages/pages.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from app.pages.endpoints import login, main
 
 pages_router = APIRouter()
-pages_router.include_router(login.router)
-pages_router.include_router(main.router)
+pages_router.include_router(login.router, tags=["pages"])
+pages_router.include_router(main.router, tags=["pages"])
