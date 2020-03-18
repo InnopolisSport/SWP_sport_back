ec103e655f10665783e10986bb6d0b1fcbc0237a
WinnerOK <danman339@gmail.com>
2020-03-18 17:43:24 +0300

    add Django admin_panel

---
+++ adminpage/sport/models/selected_priority.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Quiz(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, null=False)
+    group = models.ForeignKey("Group", on_delete=models.CASCADE, null=False)
+    priority = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], null=False)
+    quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        db_table = "selected_priority"
+        constraints = [
+            models.UniqueConstraint(fields=["user_id", "group_id", "quiz_id"], name="unique_priority_vote")
+        ]
+
+    def __str__(self):
+        return f"{self.user.email} {self.group.name}-{self.priority}"
