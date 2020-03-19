00ba731271b954907675946c3b73975a9c8f7789
DmitriyPodpryatov <podp2000@gmail.com>
2020-03-19 21:06:43 +0300

    Send submission request, generate page

--- backend/app/app/pages/pages.py
+++ backend/app/app/pages/pages.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.pages.endpoints import login, main
+from app.pages.endpoints import login, main, survey
 
 pages_router = APIRouter()
 pages_router.include_router(login.router, tags=["pages"])
 pages_router.include_router(main.router, tags=["pages"])
+pages_router.include_router(survey.router, tags=["pages"])
