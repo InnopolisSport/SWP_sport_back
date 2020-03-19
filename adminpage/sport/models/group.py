1745ebde15bd9a976b975dc589a0e6738981aaa9
WinnerOK <danman339@gmail.com>
2020-03-19 19:19:43 +0300

    fix db import

--- adminpage/sport/models/group.py
+++ adminpage/sport/models/group.py
@@ -4,7 +4,7 @@ from django.db import models
 class Group(models.Model):
     name = models.CharField(max_length=50, null=False)
     sport_id = models.ForeignKey('Sport', on_delete=models.CASCADE)
-    trainer_id = models.ForeignKey('User', on_delete=models.SET_NULL)
+    trainer_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = "group"
