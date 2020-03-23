3d3ea9cce2fc88ce50b692553388bf3f52e519b1
WinnerOK <danman339@gmail.com>
2020-03-23 12:50:58 +0300

    Auto stash before merge of "Survey" and "master"

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -7,6 +7,7 @@ from app.api.api_v1.endpoints import login, fake_login  # ,items
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(fake_login.router, prefix="/fake_login", tags=["fake-login"])
+api_router.include_router(survey.router, tags=['router'])
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
 # # api_router.include_router(items.router, prefix="/items", tags=["items"])
 # api_router.include_router(ping.router, prefix="/ping", tags=["test"])
