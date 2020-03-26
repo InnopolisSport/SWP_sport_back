656b5a4a08e267cde3ffd8df188e9a17d7edbe9c
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-03-26 20:44:21 +0300

    Fix migrations

--- adminpage/sport/models/group.py
+++ adminpage/sport/models/group.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Group(models.Model):
     name = models.CharField(max_length=50, null=False)
-    description = models.CharField(max_length=1000, null=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    capacity = models.IntegerField(default=50, null=False)
     sport = models.ForeignKey('Sport', on_delete=models.CASCADE, null=False)
     semester = models.ForeignKey('Semester', on_delete=models.CASCADE, null=False)
-    trainer = models.ForeignKey('Trainer', on_delete=models.SET_NULL, null=True)
+    trainer = models.ForeignKey('Trainer', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         db_table = "group"
