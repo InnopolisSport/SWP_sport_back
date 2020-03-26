12f23facc5ad19abb598433eb2a6d57dc2ef8533
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-03-26 21:24:16 +0300

    Fix trainings model

--- adminpage/sport/models/training.py
+++ adminpage/sport/models/training.py
@@ -2,23 +2,13 @@ from django.db import models
 
 
 class Training(models.Model):
-    class Weekdays(models.IntegerChoices):
-        MONDAY = 0, "monday",
-        TUESDAY = 1, "tuesday"
-        WEDNESDAY = 2, "wednesday"
-        THURSDAY = 3, "thursday"
-        FRIDAY = 4, "friday"
-        SATURDAY = 5, "saturday"
-        SUNDAY = 6, "sunday"
-
     group = models.ForeignKey("Group", on_delete=models.CASCADE)
-    weekday = models.IntegerField(choices=Weekdays.choices, null=False)
-    start = models.TimeField(null=False)
-    end = models.TimeField(null=False)
+    start = models.DateTimeField(null=False)
+    end = models.DateTimeField(null=False)
 
     class Meta:
         db_table = "training"
         verbose_name_plural = "trainings"
 
     def __str__(self):
-        return f"{self.group.name}: {self.weekday}"
+        return f"{self.group.name}"
