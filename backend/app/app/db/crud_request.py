a4425286eaeba3911f501c5ba6270bb9d2c6e77a
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-03-26 21:46:08 +0300

    Fix crud operations

--- backend/app/app/db/crud_request.py
+++ backend/app/app/db/crud_request.py
@@ -1,16 +1,18 @@
+from datetime import datetime
 from typing import List, Tuple
-from ..models.group_request import GroupRequest
+from ..models.group_request import GroupRequest, RequestStatus, RequestType
 
 
-def __tuple_to_request(row: Tuple[int, int, int, str, int, str]) -> GroupRequest:
-    id, group_id, student_id, created_timestamp, status, last_updated = row
+def __tuple_to_request(row: Tuple[int, int, int, datetime, int, int, datetime]) -> GroupRequest:
+    id, group_id, student_id, created, status, request_type, last_update = row
     return GroupRequest(
         id=id,
         group_id=group_id,
         student_id=student_id,
-        created_timestamp=created_timestamp,
+        created=created,
         status=status,
-        last_updated=last_updated
+        type=request_type,
+        last_update=last_update
     )
 
 
@@ -22,25 +24,27 @@ def get_student_requests(conn, student_id: int) -> List[GroupRequest]:
     @return list of all group requests
     """
     cursor = conn.cursor()
-    cursor.execute('SELECT id, group_id, student_id, created_timestamp, status, last_updated'
+    cursor.execute('SELECT id, group_id, student_id, created, status, type, last_update '
                    'FROM group_request WHERE student_id = %s', (student_id,))
     rows = cursor.fetchall()
     return list(map(__tuple_to_request, rows))
 
 
-def create_request(conn, student_id: int, group_id: int):
+def create_request(conn, student_id: int, group_id: int, type: RequestType):
     """
     Creates new group request for given student and group id
     @param conn - Database connection
     @param student_id - given student id
     @param group_id - given group id
+    @param type - request type for new request
     """
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO group_request (student_id, group_id) VALUES (%s, %s)', (student_id, group_id))
+    cursor.execute('INSERT INTO group_request (student_id, group_id, type, status, last_update, created) '
+                   'VALUES (%s, %s, %s, 0, now(), now())', (student_id, group_id, type.value))
     conn.commit()
 
 
-def update_request_status(conn, request_id: int, status: int):
+def update_request_status(conn, request_id: int, status: RequestStatus):
     """
     Creates new group request for given student and group id
     @param conn - Database connection
@@ -48,5 +52,5 @@ def update_request_status(conn, request_id: int, status: int):
     @param status - new status
     """
     cursor = conn.cursor()
-    cursor.execute('UPDATE group_request SET last_updated = now(), status = %s WHERE id = %s', (status, request_id))
+    cursor.execute('UPDATE group_request SET last_update = now(), status = %s WHERE id = %s', (status.value, request_id))
     conn.commit()
--- backend/app/app/models/group_request.py
+++ backend/app/app/models/group_request.py
@@ -1,10 +1,24 @@
+from enum import Enum
+from datetime import datetime
 from .base import Base
+
+
+class RequestType(int, Enum):
+    GROUP_ENROLL = 0
+    GROUP_LEAVE = 1
+
+
+class RequestStatus(int, Enum):
+    OPEN = 0
+    ACCEPTED = 1
+    REJECTED = 2
 
 
 class GroupRequest(Base):
     id: int
     group_id: int
     student_id: int
-    created_timestamp: str
-    status: int
-    last_updated: str
+    created: datetime
+    status: RequestStatus
+    type: RequestType
+    last_update: datetime
