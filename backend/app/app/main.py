ea11a7a975156be203e21bb68affd92394af1dcf
WinnerOK <danman339@gmail.com>
2020-04-03 11:28:04 +0300

    Fix 500 on exception raise.

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI, status, responses
+from fastapi.exception_handlers import http_exception_handler as default_http_exception_handler
 from fastapi.exceptions import HTTPException
 from fastapi.staticfiles import StaticFiles
 from starlette.requests import Request
@@ -23,7 +24,6 @@ app.include_router(api_router, prefix=config.API_V1_STR)
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
-    logger.debug(request.headers)
     request.state.db = create_connection()
     response = await call_next(request)
     request.state.db.close()
@@ -35,4 +35,4 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     if exc.status_code == status.HTTP_401_UNAUTHORIZED:
         return responses.RedirectResponse(url=f"/api/login?state=login&redirect_uri={request.url}")
     else:
-        raise exc
+        return await default_http_exception_handler(request, exc)
