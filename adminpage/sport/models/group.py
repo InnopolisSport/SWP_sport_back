01896b0e3a3aae241cbda73519a59d77dd965845
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-04-04 17:25:45 +0300

    Add db migration with is_club, special fields

--- adminpage/sport/models/group.py
+++ adminpage/sport/models/group.py
@@ -5,6 +5,7 @@ class Group(models.Model):
     name = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=1000, null=True, blank=True)
     capacity = models.IntegerField(default=50, null=False)
+    is_club = models.BooleanField(default=False, null=False)
     sport = models.ForeignKey('Sport', on_delete=models.CASCADE, null=False)
     semester = models.ForeignKey('Semester', on_delete=models.CASCADE, null=False)
     trainer = models.ForeignKey('Trainer', on_delete=models.SET_NULL, null=True, blank=True)
