9e0be6ca6c6f00d5d6f76f5145749a2f52ec6493
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-04-09 22:28:20 +0300

    Add datetime fiedls in semester scheme

--- adminpage/sport/models/semester.py
+++ adminpage/sport/models/semester.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+
 from django.db import models
 
 
 class Semester(models.Model):
     name = models.CharField(max_length=50, null=False, unique=True)
+    start = models.DateTimeField(null=False, default=datetime.now)
+    end = models.DateTimeField(null=False)
+    choice_deadline = models.DateTimeField(null=False)
 
     class Meta:
         db_table = "semester"
