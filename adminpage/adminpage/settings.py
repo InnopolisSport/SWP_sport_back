0de0b6e5ec1ee348efd1e73bb4753e14b6394967
WinnerOK <danman339@gmail.com>
2020-04-12 13:41:25 +0300

    Changed and hide a secret in Django #SPORT-77 State: To Verify Fixed problem in django

--- adminpage/adminpage/settings.py
+++ adminpage/adminpage/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z%zmsh#n6os&g45@(#6k=xl38z=_d4jz$4cct-zole3jjb9j5h'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
