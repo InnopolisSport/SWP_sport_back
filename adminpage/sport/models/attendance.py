b69d7a0cf808744bb2e0b7434167a058828f14de
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-04-14 19:03:11 +0300

    Add django and sql constraints

--- adminpage/sport/models/attendance.py
+++ adminpage/sport/models/attendance.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_hours(hours):
+    if hours <= 0:
+        raise ValidationError('Only positive values are allowed')
 
 
 class Attendance(models.Model):
     training = models.ForeignKey('Training', on_delete=models.CASCADE)
     student = models.ForeignKey("Student", on_delete=models.CASCADE)
-    hours = models.DecimalField(max_digits=3, decimal_places=2, default=1)
+    hours = models.DecimalField(max_digits=3, decimal_places=2, default=1, validators=[validate_hours])
 
     class Meta:
         db_table = "attendance"
