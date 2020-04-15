171a081b60cd868eabf794fe8ee97047e9e50b34
DmitriyPodpryatov <podp2000@gmail.com>
2020-04-15 19:14:03 +0300

    Sport schedule was added. Code refactoring.#SPORT-67

--- backend/app/app/static/js/profile.js
+++ backend/app/app/static/js/profile.js
@@ -27,3 +27,117 @@ function toggle_ill(elem) {
 
 
 }
+
+/*
+    Calendar
+*/
+const colors = [
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#9467bd',
+    '#8c564b',
+    '#b4005a',
+    '#7f7f7f',
+    '#7b7c1f',
+    '#157786',
+];
+
+const color_limit = colors.length;
+var color_ptr = 0;
+
+var group_colors = {};
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function get_color(group) {
+    if (group_colors.hasOwnProperty(group)) {
+        // Keep it to assign consistent colors
+    } else if (color_ptr < color_limit) {
+        // Start assigning colors from the list
+        group_colors[group] = colors[color_ptr];
+        color_ptr += 1;
+        return group_colors[group];
+    } else {
+        // If it happen that we finished the list - assign random colors
+        group_colors[group] = getRandomColor();
+    }
+    return group_colors[group];
+}
+
+
+function render(info) {
+    let element = info.el;
+    let event = info.event;
+
+    let props = event.extendedProps;
+    element.style.fontSize = "99";
+    element.style.backgroundColor = get_color(props.id)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['timeGrid'],
+        defaultView: 'timeGridWeek',
+        header: {
+            left: '',
+            center: '',
+            right: ''
+            // right: 'today, prev, next'
+        },
+        height: 'auto',
+        timeZone: 'Europe/Moscow',
+        firstDay: 1,
+        allDaySlot: false,
+        slotDuration: '00:30:00',
+        minTime: '08:00:00',
+        maxTime: '21:00:00',
+        defaultTimedEventDuration: '01:30',
+        eventRender: render,
+        // Event format: yyyy-mm-dd
+        // TODO: at backend use a loop of 10 standard colors as matplotlib do ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+        // TODO: show only students' personal events
+        events: '/api/calendar/' + calendarEl.getAttribute('data-sport') + '/schedule',
+        eventSources: [
+
+             // your event source
+             {
+                 events: [ // put the array in the `events` property
+                     {
+                         title: 'G1',
+                         start: '2020-04-15T08:30:00',
+                         end: '2020-04-15T10:00:00',
+                         group_id: 1
+                     }
+                 ]
+             },
+
+             {
+                 events: [ // put the array in the `events` property
+                     {
+                         title: 'G2',
+                         start: '2020-04-16T09:15:00',
+                         end: '2020-04-16T10:45:00',
+                         group_id: 2
+                     }
+                 ]
+             }
+
+             // any other event sources...
+
+         ]
+    });
+
+    calendar.render();
+});
