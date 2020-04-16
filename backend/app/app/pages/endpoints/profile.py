4aa12143c4bd07d7365a7136889d267c0ecd99f7
DmitriyPodpryatov <podp2000@gmail.com>
2020-04-16 21:00:39 +0300

    Show hours from db #SPORT-66

--- backend/app/app/pages/endpoints/profile.py
+++ backend/app/app/pages/endpoints/profile.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Request, Depends, responses
 
 from app.api.utils.db import get_db
 from app.api.utils.security import get_current_user_optional
-from app.db import get_student_main_group, find_student, get_ongoing_semester
+from app.db import get_student_main_group, find_student, get_ongoing_semester, get_brief_hours
 from app.models.user import TokenUser
 from app.pages.utils.auth import logout
 from app.pages.utils.jinja import templates
@@ -29,6 +29,9 @@ def login_page(request: Request,
         sport_group = group.qualified_name if group is not None else None
         semester = get_ongoing_semester(db)
 
+        student_hours = get_brief_hours(db, student_id)
+        [hours] = [sem.hours for sem in student_hours if sem.semester_id == semester.id]
+
         return templates.TemplateResponse("profile.html", {
             "request": request,
             "user": current_user,
@@ -39,6 +42,7 @@ def login_page(request: Request,
             "student": {
                 "sport_id": sport_id,
                 "sport_group": sport_group,
+                "hours": hours,
                 **(student.dict())
             }
         })
