dc2098bfcaf12251fa91770120c53becd698fb1d
WinnerOK <danman339@gmail.com>
2020-04-29 10:32:43 +0300

    export enrollment to xlsx

--- adminpage/sport/admin/mixins.py
+++ adminpage/sport/admin/mixins.py
@@ -1,23 +1,23 @@
-import csv
-
 from django.http import HttpResponse
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
 
 from sport.models import Semester
 
 
-class EnrollExportCsvMixin:
+class EnrollExportXlsxMixin:
     def export_as_csv(self, request, queryset):
         field_names = ["Sport", "Fullname", "Email"]
-        response = HttpResponse(content_type='text/csv')
         semester_id = request.GET.get("semester")
+        work_book = Workbook(write_only=True)
         if semester_id is None:
             semester_name = "all"
         else:
             semester_name = Semester.objects.get(pk=semester_id).name
-        response['Content-Disposition'] = f'attachment; filename=SportEnrollment_{semester_name}.csv'
-        writer = csv.writer(response)
 
-        writer.writerow(field_names)
+        work_sheet = work_book.create_sheet(title=semester_name)
+        work_sheet.append(field_names)
+
         for obj in queryset:
             if not obj.is_primary:
                 continue
@@ -27,8 +27,11 @@ class EnrollExportCsvMixin:
 
             student_fullname = str(obj.student)
             student_email = obj.student.email
+            work_sheet.append([sport_name, student_fullname, student_email])
 
-            writer.writerow([sport_name, student_fullname, student_email])
+        response = HttpResponse(save_virtual_workbook(work_book),
+                                content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename=SportEnrollment_{semester_name}.xlsx'
 
         return response
 
