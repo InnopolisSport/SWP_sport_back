a6e4fd77a14b03799204244ba72cbabfe7d634e3
WinnerOK <danman339@gmail.com>
2020-05-31 09:44:26 +0300

    clear db for test

--- adminpage/sport/tests/test_user.py
+++ adminpage/sport/tests/test_user.py
@@ -5,19 +5,22 @@ from django.core.management import call_command
 from sport.fixtures.user_creation import *
 from sport.models import Student
 
+
 @pytest.mark.django_db
 def test_student_create(user_factory):
     user = user_factory("A")
     group = Group.objects.get_or_create(
-        verbose_name = "Students",
+        verbose_name="Students",
         name="S-1-5-21-721043115-644155662-3522934251-2285",
     )
+    from pprint import pprint
+    pprint(user.groups)
     user.groups.add(group)
     assert User.objects.count() == 1
     assert Student.objects.count() == 1
 
 # def test_student_delete(db, student_factory):
 #     call_command("loaddata", "sport/fixtures/001_created_student.json")
-    
+
 #     assert User.objects.count() == 1
 #     assert Student.objects.count() == 1
