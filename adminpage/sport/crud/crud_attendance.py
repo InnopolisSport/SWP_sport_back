ebe02fd30bae8c665a0967a150beb7307060c722
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-06-02 16:18:25 +0300

    Migrate attendace cruds to django

---
+++ adminpage/sport/crud/crud_attendance.py
@@ -0,0 +1,83 @@
+from math import floor
+from typing import Iterable, Tuple
+
+from django.db import connection
+
+from sport.crud.utils import dictfetchall
+from sport.models import Student, Semester, Training
+
+
+def get_brief_hours(student: Student):
+    """
+    Retrieves statistics of hours per different semesters
+    """
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT '
+                       's.id AS semester_id, '
+                       's.name AS semester_name, '
+                       's.start AS semester_start, '
+                       's.end AS semester_end, '
+                       'sum(a.hours) AS hours '
+                       'FROM semester s, training t, "group" g, attendance a '
+                       'WHERE a.student_id = %s '
+                       'AND a.training_id = t.id '
+                       'AND t.group_id = g.id '
+                       'AND g.semester_id = s.id '
+                       'GROUP BY s.id', (student.pk,))
+        return dictfetchall(cursor)
+
+
+def get_detailed_hours(student: Student, semester: Semester):
+    """
+    Retrieves statistics of hours in one semester
+    """
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT g.name AS "group", t.start AS "timestamp", a.hours AS hours '
+                       'FROM training t, "group" g, attendance a '
+                       'WHERE a.student_id = %s '
+                       'AND a.training_id = t.id '
+                       'AND t.group_id = g.id '
+                       'AND g.semester_id = %s', (student.pk, semester.pk))
+        return dictfetchall(cursor)
+
+
+def mark_hours(training: Training, student_hours: Iterable[Tuple[int, float]]):
+    """
+    Puts hours for one training session to one student. If hours for session were already put, updates it
+    @param training: given training
+    @param student_hours: iterable with items (<student_id:int>, <student_hours:float>)
+    """
+    for student_id, student_mark in student_hours:
+        if student_id <= 0 or student_mark < 0.0:
+            raise ValueError(f"All students id and marks must be non-negative, got {(student_id, student_mark)}")
+        # Currently hours field is numeric(3,2), so
+        # A field with precision 3, scale 2 must round to an absolute value less than 10^1.
+        floor_max = 10
+        if floor(student_mark) >= floor_max:
+            raise ValueError(f"All students marks must floor to less than {floor_max}, "
+                             f"got {student_mark} -> {floor(student_mark)} >= {floor_max}")
+    with connection.cursor() as cursor:
+        args_add_str = b",".join(
+            cursor.mogrify("(%s, %s, %s)", (student_id, training.pk, student_mark))
+            for student_id, student_mark in student_hours if student_mark > 0
+        )
+        args_del_str = b",".join(
+            cursor.mogrify("(%s, %s)", (student_id, training.pk))
+            for student_id, student_mark in student_hours if student_mark == 0
+        )
+        if len(args_add_str) > 0:
+            cursor.execute(f'INSERT INTO attendance (student_id, training_id, hours) VALUES {args_add_str.decode()} '
+                           f'ON CONFLICT ON CONSTRAINT unique_attendance '
+                           f'DO UPDATE set hours=excluded.hours')
+        if len(args_del_str) > 0:
+            cursor.execute(f'DELETE FROM attendance '
+                           f'WHERE  (student_id, training_id) IN ({args_del_str.decode()})')
+
+
+def toggle_illness(student: Student):
+    """
+    Toggles student's illness
+    """
+    student.is_ill = not student.is_ill
+    student.save()
+
---
+++ adminpage/sport/tests/test_attendance_cruds.py
@@ -0,0 +1,106 @@
+import pytest
+from django.utils import timezone
+
+from sport.crud.crud_attendance import *
+from sport.models import Student, Attendance, Semester, Group, Training, Sport
+
+
+@pytest.mark.django_db
+def test_hours_statistics(student_factory):
+    student_factory("A")
+    student_factory("B")
+    student, other_student = list(Student.objects.all())
+    sport = Sport.objects.create(name="Sport")
+    s1 = Semester.objects.create(name="S19")
+    s2 = Semester.objects.create(name="S20")
+    g11 = Group.objects.create(name="G11", sport=sport, semester=s1)
+    g12 = Group.objects.create(name="G12", sport=sport, semester=s1)
+    g21 = Group.objects.create(name="G21", sport=sport, semester=s2)
+    g22 = Group.objects.create(name="G22", sport=sport, semester=s2)
+
+    trainings = []
+    for i, g in enumerate([g11, g12, g21, g22]):
+        t = Training.objects.create(group=g, start=timezone.now(), end=timezone.now())
+        trainings.append(t)
+        Attendance.objects.create(training=t, student=student, hours=i + 1)
+        Attendance.objects.create(training=t, student=other_student, hours=1)
+    brief = get_brief_hours(student)
+    assert brief == [
+        {
+            "hours": 3,
+            "semester_id": s1.pk,
+            "semester_name": s1.name,
+            "semester_start": s1.start,
+            "semester_end": s1.end
+        },
+        {
+            "hours": 7,
+            "semester_id": s2.pk,
+            "semester_name": s2.name,
+            "semester_start": s2.start,
+            "semester_end": s2.end
+        }
+    ]
+    stat1 = get_detailed_hours(student, s1)
+    stat2 = get_detailed_hours(student, s2)
+    assert stat1 == [
+        {
+            "group": g12.name,
+            "timestamp": trainings[1].start,
+            "hours": 2
+        },
+        {
+            "group": g11.name,
+            "timestamp": trainings[0].start,
+            "hours": 1
+        }
+    ]
+    assert stat2 == [
+        {
+            "group": g22.name,
+            "timestamp": trainings[3].start,
+            "hours": 4
+        },
+        {
+            "group": g21.name,
+            "timestamp": trainings[2].start,
+            "hours": 3
+        }
+    ]
+
+
+@pytest.mark.django_db
+def test_mark_hours(student_factory):
+    student_factory("A")
+    student_factory("B")
+    student1, student2 = list(Student.objects.all())
+    sport = Sport.objects.create(name="Sport")
+    semester = Semester.objects.create(name="S19")
+    group = Group.objects.create(name="G1", sport=sport, semester=semester)
+    training = Training.objects.create(group=group, start=timezone.now(), end=timezone.now())
+    mark_hours(training, [
+        (student1.pk, 1.5),
+        (student2.pk, 2.5)
+    ])
+    attendance1 = Attendance.objects.get(student=student1)
+    attendance2 = Attendance.objects.get(student=student2)
+    assert attendance1.hours == 1.5
+    assert attendance2.hours == 2.5
+    mark_hours(training, [
+        (student1.pk, 3.5),
+        (student2.pk, 0)
+    ])
+    attendance1 = Attendance.objects.get(student=student1)
+    assert attendance1.hours == 3.5
+    assert Attendance.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_toggle_illness(student_factory):
+    student_factory("A")
+    student = Student.objects.get()
+    assert not student.is_ill
+    toggle_illness(student)
+    assert student.is_ill
+    toggle_illness(student)
+    assert not student.is_ill
