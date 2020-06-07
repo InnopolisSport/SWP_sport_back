a9835a93f4ca23084bcad88fe7cd75aac2995d19
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-06-07 14:58:09 +0300

    Add semester admin page

--- adminpage/sport/admin/semesterAdmin.py
+++ adminpage/sport/admin/semesterAdmin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
 from sport.models import Semester
+from .utils import year_filter
 
 
 @admin.register(Semester)
 class SemesterAdmin(admin.ModelAdmin):
-    pass
+    search_fields = (
+        "name",
+        "start",
+    )
+
+    ordering = (
+        "-start",
+    )
+
+    list_filter = (
+        year_filter(Semester, 'start__year'),
+    )
+
+    list_display = (
+        "name",
+        "start",
+        "end",
+        "choice_deadline",
+    )
--- adminpage/sport/admin/utils.py
+++ adminpage/sport/admin/utils.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import models
 
 
 def custom_titled_filter(title, filter_class=admin.RelatedFieldListFilter):
@@ -9,3 +10,18 @@ def custom_titled_filter(title, filter_class=admin.RelatedFieldListFilter):
             return instance
 
     return Wrapper
+
+
+def year_filter(model: models.Model, year_field: str):
+    class YearFilter(admin.SimpleListFilter):
+        title = 'study year'
+        parameter_name = year_field
+
+        def lookups(self, request, model_admin):
+            years = list(model.objects.all().values(year_field).distinct())
+            return list(map(lambda x: (x[year_field], x[year_field]), years))
+
+        def queryset(self, request, queryset):
+            if self.value() is not None:
+                return queryset.filter(start__year=self.value())
+    return YearFilter
