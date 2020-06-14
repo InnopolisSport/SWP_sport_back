f144b7493a703a1133b8279270ac624a2f1cc360
WinnerOK <danman339@gmail.com>
2020-06-14 11:30:35 +0300

    Separate mixins, export hours

--- adminpage/sport/admin/semesterAdmin.py
+++ adminpage/sport/admin/semesterAdmin.py
@@ -1,6 +1,55 @@
-from django.contrib import admin
+from sys import stderr
 
-from sport.models import Semester
+from django.contrib import admin, messages
+from django.contrib.auth import get_user_model
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from django.http import HttpResponse
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
+
+from sport.models import Semester, Attendance, Student
+
+
+def export_hours_as_xlsx(modeladmin, request, queryset):
+    field_names = ["Name", "Surname", "Email", "Hours"]
+    work_book = Workbook(write_only=True)
+
+    for semester in queryset:
+        # student_hours = Student.objects\
+        #     .filter(attendance__training__group__semester=semester)\
+        #     .annotate(hours=Coalesce(Sum("attendance__hours"), 0))
+        student_hours = get_user_model().objects.raw(
+            "select u.id "
+            "     , u.first_name "
+            "     , u.last_name "
+            "     , u.email "
+            "     , coalesce(sum(a.hours), 0) as hours "
+            "from student s "
+            "         join auth_user u on s.user_id = u.id "
+            "         left join ( "
+            "    select * "
+            "    from attendance a "
+            "             join training t on a.training_id = t.id "
+            "             join \"group\" g on t.group_id = g.id "
+            "    where g.semester_id = %s "
+            ") as a on s.id = a.student_id "
+            "where s.user_id = u.id "
+            "group by s.id, u.id "
+            "order by u.last_name, u.first_name; ", [semester.pk])
+        work_sheet = work_book.create_sheet(title=semester.name)
+        work_sheet.append(field_names)
+        for student in student_hours:
+            work_sheet.append([student.first_name, student.last_name, student.email, student.hours])
+
+    response = HttpResponse(save_virtual_workbook(work_book),
+                            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f'attachment; filename=SportHours_{"_".join(map(str, queryset.all()))}.xlsx'
+
+    return response
+
+
+export_hours_as_xlsx.short_description = "Export hours for selected semesters"
 
 
 @admin.register(Semester)
@@ -19,4 +68,8 @@ class SemesterAdmin(admin.ModelAdmin):
         "start",
         "end",
         "choice_deadline",
+    )
+
+    actions = (
+        export_hours_as_xlsx,
     )
