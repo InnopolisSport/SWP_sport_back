a6b8735eff9798a27f397353066f4209f99a795b
WinnerOK <danman339@gmail.com>
2020-06-24 09:16:14 +0300

    Basic import

--- adminpage/sport/admin/studentAdmin.py
+++ adminpage/sport/admin/studentAdmin.py
@@ -1,12 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from import_export.widgets import ForeignKeyWidget
 
 from sport.models import Student
 from .inlines import AttendanceInline
 from .utils import user__email
+from import_export import resources, fields
+from import_export.admin import ImportMixin
+
+from sport.signals import get_or_create_student_group
+
+
+class StudentResource(resources.ModelResource):
+
+    def before_import(self, dataset, dry_run, *args, **kwargs):
+        user_ids = []
+        for data in dataset.dict:
+            email = data["email"]
+
+            user, _ = get_user_model().objects.get_or_create(
+                email=email,
+                defaults={
+                    "first_name": data["first_name"],
+                    "last_name": data["last_name"],
+                    "username": data["email"],
+                }
+            )
+
+            user.groups.add(get_or_create_student_group())
+            user_ids.append(user.pk)
+        dataset.insert_col(0, user_ids, "user")
+        return super(StudentResource, self).before_import(dataset, dry_run, *args, **kwargs)
+
+    class Meta:
+        model = Student
+        fields = (
+            "user",
+            "email",
+            "first_name",
+            "last_name",
+            "is_ill",
+            "medical_group",
+        )
+        import_id_fields = ("user",)
 
 
 @admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = StudentResource
+
     search_fields = (
         "user__first_name",
         "user__last_name",
