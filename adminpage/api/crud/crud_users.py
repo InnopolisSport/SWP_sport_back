d364d762a67d3f5d7e8e00582331c9eb56f0ef08
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-06-30 13:29:46 +0500

    Add text search index for user

--- adminpage/api/crud/crud_users.py
+++ adminpage/api/crud/crud_users.py
@@ -17,10 +17,10 @@ def get_email_name_like_students(pattern: str, limit: int = 5):
                        'd.first_name AS first_name, '
                        'd.last_name AS last_name, '
                        'd.email AS email,'
-                       'concat(d.first_name, \' \', d.last_name) as full_name '
+                       'd.first_name || \' \' || d.last_name as full_name '
                        'FROM auth_user d '
                        'WHERE d.email LIKE %s or '
-                       'lower(concat(d.first_name, \' \', d.last_name)) LIKE %s or '
+                       'lower(d.first_name || \' \' || d.last_name) LIKE %s or '
                        'lower(d.last_name) LIKE %s '
                        'LIMIT %s',
                        (pattern, pattern, pattern, str(limit)))
