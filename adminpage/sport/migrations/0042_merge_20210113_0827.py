1a74d5a38939717a8db867eb1ba9e69273b78ef2
WinnerOK <danman339@gmail.com>
2021-01-13 11:43:18 +0300

    merge migrations with dev

--- adminpage/sport/migrations/0042_merge_20210113_0827.py
+++ adminpage/sport/migrations/0042_merge_20210113_0827.py
@@ -8,6 +8,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('sport', '0041_grant_grafana_permissions'),
         ('sport', '0040_student_telegram'),
+        ('sport', '0041_auto_20210109_1308'),
     ]
 
     operations = [
