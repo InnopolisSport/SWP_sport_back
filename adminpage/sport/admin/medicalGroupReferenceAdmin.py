39647241ae2857ce0ddfa68d0fcd798d29a1d3c8
WinnerOK <danman339@gmail.com>
2021-02-07 18:13:45 +0300

    Show semester at medical group reference admin

--- adminpage/sport/admin/medicalGroupReferenceAdmin.py
+++ adminpage/sport/admin/medicalGroupReferenceAdmin.py
@@ -4,7 +4,11 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from sport.admin import site
-from sport.models import MedicalGroupReference, MedicalGroup, StudentMedicalGroup
+from sport.models import (
+    MedicalGroupReference,
+    MedicalGroup,
+    StudentMedicalGroup,
+)
 
 
 class StudentTextFilter(AutocompleteFilter):
@@ -16,14 +20,21 @@ class MedicalGroupReferenceForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance.student_id is not None:
-            self.fields['medical_group'].initial = self.instance.student.medical_group_id
+            self.fields[
+                'medical_group'].initial = \
+                self.instance.student.medical_group_id
 
-    medical_group = forms.ModelChoiceField(MedicalGroup.objects.all(), initial=-2)
+    medical_group = forms.ModelChoiceField(
+        MedicalGroup.objects.all(),
+        initial=-2
+    )
 
     def clean_comment(self):
         if self.cleaned_data['medical_group'].pk == -2 and \
                 self.cleaned_data['comment'] == '':
-            raise forms.ValidationError('Please, specify reject reason in the comment field')
+            raise forms.ValidationError(
+                'Please, specify reject reason in the comment field'
+            )
         return self.cleaned_data['comment']
 
     def save(self, commit=True):
@@ -31,7 +42,7 @@ class MedicalGroupReferenceForm(forms.ModelForm):
         obj, created = StudentMedicalGroup.objects.get_or_create(
             student=self.instance.student,
             semester=self.instance.semester,
-            defaults={ 'medical_group_id': medical_group_id }
+            defaults={'medical_group_id': medical_group_id}
         )
         if not created:
             obj.medical_group_id = self.cleaned_data['medical_group'].pk
@@ -55,33 +66,38 @@ class MedicalGroupReferenceAdmin(admin.ModelAdmin):
 
     list_display = (
         "student",
-        "image",
+        "semester",
         "resolved",
     )
 
     list_select_related = (
         "student",
         "student__user",
+        "semester",
     )
 
     list_filter = (
         "resolved",
+        "semester",
         StudentTextFilter,
     )
 
     fields = (
         "student",
+        "semester",
         ("medical_group", "comment"),
         "reference_image",
     )
 
     readonly_fields = (
         "student",
+        "semester",
         "reference_image",
     )
 
     def save_model(self, request, obj, form, change):
-        if obj.resolved is None or 'comment' in form.changed_data or 'medical_group' in form.changed_data:
+        if obj.resolved is None or 'comment' in form.changed_data \
+                or 'medical_group' in form.changed_data:
             obj.resolved = form.cleaned_data['medical_group'].pk > -2
             super().save_model(request, obj, form, change)
 
@@ -89,7 +105,11 @@ class MedicalGroupReferenceAdmin(admin.ModelAdmin):
         return False
 
     def reference_image(self, obj):
-        return format_html('<a href="{}"><img style="width: 50%" src="{}" /></a>', obj.image.url, obj.image.url)
+        return format_html(
+            '<a href="{}"><img style="width: 50%" src="{}" /></a>',
+            obj.image.url,
+            obj.image.url
+        )
 
     reference_image.short_description = 'Reference'
     reference_image.allow_tags = True
