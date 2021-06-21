99fd64e6e1d53cc85ad885803f37e0aa4775a6ba
Kirill-Kuznetsov-git <kir.cuzneczo2012@yandex.ru>
2021-06-21 23:02:58 +0300

    update permission ""IsStudent

--- adminpage/api/permissions.py
+++ adminpage/api/permissions.py
@@ -2,13 +2,14 @@ from django.conf import settings
 
 from rest_framework import permissions
 
+from django.contrib.auth import get_user_model
+
+from sport.models import Student
+
 
 class IsStudent(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user and \
-                request.user.groups.filter(
-                    verbose_name=settings.STUDENT_AUTH_GROUP_VERBOSE_NAME
-                ):
+        if str(Student.objects.filter(user=get_user_model().objects.filter(email=request.user.email)[0])[0].student_status) == 'Normal' and hasattr(request.user, 'student'):
             return True
         return False
 
