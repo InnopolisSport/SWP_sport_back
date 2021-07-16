98d16ccf1cf275de6650d8b9b35f2391456f2435
danil <andreevdanil35@gmail.com>
2021-07-16 22:22:13 +0800

    SPORT-278: extra attendance restriction

--- adminpage/adminpage/settings.py
+++ adminpage/adminpage/settings.py
@@ -92,7 +92,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv_boolean("DEBUG")
 PROJECT_ROOT = "/src/"
-ALLOWED_HOSTS = [HOSTNAME, ]
+ALLOWED_HOSTS = ["0.0.0.0", ]
 
 if DEBUG:
     ALLOWED_HOSTS.append('localhost')
