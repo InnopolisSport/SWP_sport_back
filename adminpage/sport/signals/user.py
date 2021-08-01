4ae663bdd5d0b044c46ce08694e18e49888e43c9
Kirill Kuznetsov <kir.cuzneczo2012@yandex.ru>
2021-08-02 00:36:59 +0400

    Update urls.py

--- adminpage/sport/signals/user.py
+++ adminpage/sport/signals/user.py
@@ -86,6 +86,7 @@ def change_status_to_academic_leave(instance: Student, sender, using, **kwargs):
     semester = get_ongoing_semester()
     if instance.student_status.name == "Academic leave":
         semester.academic_leave_students.add(instance)
+        semester.save()
 
 
 def update_group_verbose_names(sid_to_name_mapping: dict):
