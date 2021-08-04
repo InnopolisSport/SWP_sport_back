c20e302a17bef52b686bb1f88f401b5ff90a05f0
kilimanj4r0 <v.makharev@yandex.ru>
2021-08-05 00:47:45 +0700

    SPORT-325: Comment whole tour

--- adminpage/sport/static/sport/js/modules/enrollment.mjs
+++ adminpage/sport/static/sport/js/modules/enrollment.mjs
@@ -2,19 +2,19 @@ async function  enroll(group_id, action, errorHandler=function(group_id){}) {
     sendResults(`/api/enrollment/${action}`, {group_id: group_id})
         .then(data => {
             goto_profile()
-            // If tour is in process and student unenrolls, having max number of groups,
-            // the page reloads together with the tour, thus updating the steps, so the step update is required
-            if (!tour.ended() && $('.tour-step-choice-btn').text().includes('No group choices left')) {
-                if (tour.getCurrentStepIndex() === 7) {
-                    tour.setCurrentStep(15);
-                }
-            } else
-                // When only two calendar steps are shown, and student has only one group (secondary - student cannot unenroll from primary group)
-                // e.g. calendar will be removed from page after reloading
-                if (!tour.ended() && $('.tour-step-choice-btn').text().includes('group choices left') &&
-                    (localStorage.getItem("main-tour_calendar") === "false")) {
-                tour.end();
-            }
+            // // If tour is in process and student unenrolls, having max number of groups,
+            // // the page reloads together with the tour, thus updating the steps, so the step update is required
+            // if (!tour.ended() && $('.tour-step-choice-btn').text().includes('No group choices left')) {
+            //     if (tour.getCurrentStepIndex() === 7) {
+            //         tour.setCurrentStep(15);
+            //     }
+            // } else
+            //     // When only two calendar steps are shown, and student has only one group (secondary - student cannot unenroll from primary group)
+            //     // e.g. calendar will be removed from page after reloading
+            //     if (!tour.ended() && $('.tour-step-choice-btn').text().includes('group choices left') &&
+            //         (localStorage.getItem("main-tour_calendar") === "false")) {
+            //     tour.end();
+            // }
         })
         .catch(function (error) {
             errorHandler(group_id);
