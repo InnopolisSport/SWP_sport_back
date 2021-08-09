a2345bc5123ce2ef4cf88efbd5ebceb0a8c44145
Artem Batalov <a@batalov.me>
2021-08-09 16:34:45 +0300

    Shorter

--- adminpage/api/permissions.py
+++ adminpage/api/permissions.py
@@ -4,14 +4,14 @@ from sport.models import Student
 
 class IsStudent(permissions.BasePermission):
     def has_permission(self, request, view):
-        if hasattr(request.user, 'student') and Student.objects.get(user=request.user).student_status == 'Normal':
+        if hasattr(request.user, 'student') and request.user.student.student_status == 'Normal':
             return True
         return False
 
 
 class SportSelected(IsStudent):
     def has_permission(self, request, view):
-        return super().has_permission(request, view) and Student.objects.get(user_id=request.user.id).sport
+        return super().has_permission(request, view) and request.user.student.sport
 
 
 class IsTrainer(permissions.BasePermission):
