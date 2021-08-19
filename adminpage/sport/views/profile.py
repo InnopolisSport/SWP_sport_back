5a3eec9b59a5d47105a5f18c1deebd53629f366a
Artem Batalov <a@batalov.me>
2021-08-20 00:08:14 +0300

    Update profile.py

--- adminpage/sport/views/profile.py
+++ adminpage/sport/views/profile.py
@@ -101,7 +101,7 @@ def profile_view(request, **kwargs):
                 "has_unresolved_med_group_submission": has_unresolved_med_group_submission,
                 **student_data,
                 "sport": student.sport,
-                "init_debt_hours": student_debt.first().debt if student_debt.exists() else None,
+                "init_debt_hours": student_debt.first().debt if student_debt.exists() else 0,
                 "debt_hours": get_negative_hours(student.pk),
                 "all_hours": get_student_hours(student.pk)['ongoing_semester'],
                 "better_than": better_than(student.pk)
