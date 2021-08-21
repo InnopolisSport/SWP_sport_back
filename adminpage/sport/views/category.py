46b1da21a14c6cd23f5bbfa2c424b64ed8213ee3
kilimanj4r0 <v.makharev@yandex.ru>
2021-08-21 16:06:16 +0300

    SPORT-394: Change alert logic and add 'club' in titles

--- adminpage/sport/views/category.py
+++ adminpage/sport/views/category.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from api.crud import get_sports, get_clubs
+from api.crud import get_sports, get_clubs, get_student_groups
+
+
+def parse_group(group: dict) -> dict:
+    return {
+        "id": group["id"],
+        'qualified_name': f'{group["name"]} ({group["sport_name"]})',
+        "name": group["name"],
+        "sport": group["sport_name"]
+    }
 
 
 @login_required
@@ -17,9 +26,15 @@ def category_view(request, **kwargs):
         key=lambda group: (group["current_load"] >= group["capacity"], group["name"])
     )
     sc_training_groups = []  # TODO
+    student_groups = get_student_groups(student)
+    student_groups_parsed = list(map(
+        parse_group,
+        student_groups
+    ))
     return render(request, "category.html", {
         "sports": sports,
         "clubs": clubs,
         "sc_training_groups": sc_training_groups,
         "student": student,
+        "student_sport_group": student_groups_parsed,
     })
