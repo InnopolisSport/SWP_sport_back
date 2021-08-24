7e5ee7bdaeacd123f1b9885725e0438012000bca
Kirill Kuznetsov <kir.cuzneczo2012@yandex.ru>
2021-08-24 18:44:07 +0300

    Update faq_category.py

--- adminpage/sport/models/faq_category.py
+++ adminpage/sport/models/faq_category.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class FAQCategory(models.Model):
     name = models.CharField(max_length=50, null=False)
-    description = models.TextField(max_length=1000, null=False)
 
     class Meta:
         db_table = "faq_category"
