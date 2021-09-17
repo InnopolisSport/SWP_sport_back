862ad950350c476ac258178ba2a55a58209b500f
Artem Batalov <a@batalov.me>
2021-09-17 19:18:04 +0300

    SPORT-398: fix test

--- adminpage/api/tests/crud/test_user.py
+++ adminpage/api/tests/crud/test_user.py
@@ -73,6 +73,7 @@ def test_get_email_name_like_students(
         "last_name": user.last_name,
         "email": user.email,
         "full_name": f"{user.first_name} {user.last_name}",
+        "medical_group__name": "Medical checkup not passed"
     }]
     assert len(get_email_name_like_students(group.id, "Kir")) == 1
     assert len(get_email_name_like_students(group.id, "Kirill Fed")) == 1
