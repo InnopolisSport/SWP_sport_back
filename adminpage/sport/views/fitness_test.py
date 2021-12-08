9fa22a15bbdf818c8304c5efd2f7a17f435065b1
kilimanj4r0 <v.makharev@yandex.ru>
2021-12-08 20:37:16 +0300

    SPORT-426: Refactor fitness test session page, add select field and save alert (merge)

--- adminpage/sport/views/fitness_test.py
+++ adminpage/sport/views/fitness_test.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 @login_required
 def fitness_test_view(request, **kwargs):
-    return render(request, "fitness_test.html")
+    return render(request, "fitness_test_session.html")
