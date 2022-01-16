c0b5683927496126410897684b24832e73ca4ff9
Artem Batalov <a@batalov.me>
2022-01-16 16:53:32 +0300

    SPORT-452: New debts and sport clean while semester creation

--- adminpage/sport/models/debt.py
+++ adminpage/sport/models/debt.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Debt(models.Model):
     student = models.ForeignKey("Student", on_delete=models.CASCADE)
-    semester = models.ForeignKey("Semester", on_delete=models.CASCADE)
+    semester = models.ForeignKey("Semester", on_delete=models.CASCADE, null=True, blank=True)
     debt = models.IntegerField(default=0)
 
     def save(self, *args, **kwargs):
