5c9d604181d35fd45e2d67ff5764f3f71911e130
Vladimir Makharev <v.makharev@yandex.ru>
2022-02-22 20:51:02 +0300

    SPORT-491: Setting default value of student hours to current max after CSV upload and formatting

--- adminpage/sport/static/sport/js/profile.js
+++ adminpage/sport/static/sport/js/profile.js
@@ -1,19 +1,19 @@
 function make_hours_table(trainings) {
-    const table = $(
-        '<table class="table table-hover table-sm table-borderless">'
-    );
-    table
-        .append('<thead>')
-        .children('thead')
-        .append('<tr />')
-        .children('tr')
-        .append(
-            '<th scope="col">Group</th><th scope="col">Date</th><th scope="col">Hours</th><th scope="col">Approved</th>'
-        );
-    const tbody = table.append('<tbody>').children('tbody');
-    trainings.forEach(({ group, custom_name, timestamp, hours, approved }) => {
-        tbody.append(
-            $(`<tr>
+	const table = $(
+		'<table class="table table-hover table-sm table-borderless">'
+	);
+	table
+		.append('<thead>')
+		.children('thead')
+		.append('<tr />')
+		.children('tr')
+		.append(
+			'<th scope="col">Group</th><th scope="col">Date</th><th scope="col">Hours</th><th scope="col">Approved</th>'
+		);
+	const tbody = table.append('<tbody>').children('tbody');
+	trainings.forEach(({ group, custom_name, timestamp, hours, approved }) => {
+		tbody.append(
+			$(`<tr>
                             <td><span class="badge badge-info text-uppercase">${
 								custom_name || group
 							}</span></td>
@@ -27,27 +27,28 @@ function make_hours_table(trainings) {
 									: 'No'
 							}</td>
                         </tr>`)
-        );
-    });
-    return table;
+		);
+	});
+	return table;
 }
 
 const loaded_hours = {};
 
 async function fetch_detailed_hours(e) {
-    const semester_id = parseInt(e.getAttribute('data-semester-id'), 10);
-    if (loaded_hours[semester_id]) return;
-    const response = await fetch(
-        `/api/profile/history_with_self/${semester_id}`, {
-            method: 'GET',
-            'X-CSRFToken': csrf_token,
-        }
-    );
-    const history = await response.json();
-    const table = $(`#hours-modal-${semester_id} .modal-body`);
-    table.empty();
-    table.append(make_hours_table(history.trainings));
-    loaded_hours[semester_id] = true;
+	const semester_id = parseInt(e.getAttribute('data-semester-id'), 10);
+	if (loaded_hours[semester_id]) return;
+	const response = await fetch(
+		`/api/profile/history_with_self/${semester_id}`,
+		{
+			method: 'GET',
+			'X-CSRFToken': csrf_token,
+		}
+	);
+	const history = await response.json();
+	const table = $(`#hours-modal-${semester_id} .modal-body`);
+	table.empty();
+	table.append(make_hours_table(history.trainings));
+	loaded_hours[semester_id] = true;
 }
 
 // function toggle_ill(elem) {
@@ -65,65 +66,65 @@ async function fetch_detailed_hours(e) {
 // }
 
 function close_modal(modal_id) {
-    $(modal_id).modal('hide');
+	$(modal_id).modal('hide');
 }
 
 function open_recovered_modal() {
-    $('#recovered-modal').modal('show');
-    $('#reference-file-input')
-        .off('change')
-        .val('')
-        .on('change', function() {
-            const parts = $(this).val().split('\\');
-            $(this)
-                .prev('.custom-file-label')
-                .html(parts[parts.length - 1]);
-        })
-        .prev('.custom-file-label')
-        .html('Reference image');
+	$('#recovered-modal').modal('show');
+	$('#reference-file-input')
+		.off('change')
+		.val('')
+		.on('change', function () {
+			const parts = $(this).val().split('\\');
+			$(this)
+				.prev('.custom-file-label')
+				.html(parts[parts.length - 1]);
+		})
+		.prev('.custom-file-label')
+		.html('Reference image');
 }
 
 function open_med_group_modal() {
-    $('#med-group-modal').modal('show');
+	$('#med-group-modal').modal('show');
 }
 
 async function open_selfsport_modal() {
-    $('#selfsport-modal').modal('show');
-    $('#self-sport-type-help').html('');
-    const options = await fetch('/api/selfsport/types')
-        .then((res) => res.json())
-        .then((arr) => arr.sort((a, b) => a.name > b.name));
-    const el = $('#self-sport-type');
-    el.children().remove();
-    el.append(
-        '<option value="" disabled selected>Select your training type</option>'
-    );
-    el.off('change').change(function(e) {
-        $('#self-sport-type-help').html(
-            options.find((option) => option.pk.toString() === e.target.value)
-            .application_rule
-        );
-    });
-    options.forEach((option) => {
-        el.append(`<option value="${option.pk}">${option.name}</option>`);
-    });
+	$('#selfsport-modal').modal('show');
+	$('#self-sport-type-help').html('');
+	const options = await fetch('/api/selfsport/types')
+		.then((res) => res.json())
+		.then((arr) => arr.sort((a, b) => a.name > b.name));
+	const el = $('#self-sport-type');
+	el.children().remove();
+	el.append(
+		'<option value="" disabled selected>Select your training type</option>'
+	);
+	el.off('change').change(function (e) {
+		$('#self-sport-type-help').html(
+			options.find((option) => option.pk.toString() === e.target.value)
+				.application_rule
+		);
+	});
+	options.forEach((option) => {
+		el.append(`<option value="${option.pk}">${option.name}</option>`);
+	});
 }
 
 async function openMedicalInfoModal(groupName, groupDescription) {
-    const { data, title, body, footer } = await openModal(
-        '#medical-group-info-modal',
-        null
-    );
-    title.text(`Medical group info - ${groupName}`);
-    body.append(groupDescription);
-    footer.html(
-        '<div class="container">' +
-        '<div class="row justify-content-between">' +
-        '<button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="open_med_group_modal()">Change medical group</button> ' +
-        '<button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>' +
-        '</div>' +
-        '</div>'
-    );
+	const { data, title, body, footer } = await openModal(
+		'#medical-group-info-modal',
+		null
+	);
+	title.text(`Medical group info - ${groupName}`);
+	body.append(groupDescription);
+	footer.html(
+		'<div class="container">' +
+			'<div class="row justify-content-between">' +
+			'<button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="open_med_group_modal()">Change medical group</button> ' +
+			'<button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>' +
+			'</div>' +
+			'</div>'
+	);
 }
 
 /*
@@ -131,122 +132,122 @@ async function openMedicalInfoModal(groupName, groupDescription) {
 */
 
 function render(info) {
-    let element = info.el;
-    let event = info.event;
+	let element = info.el;
+	let event = info.event;
 
-    let props = event.extendedProps;
-    element.style.fontSize = '99';
-    element.style.backgroundColor = get_color(event.title);
-    element.style.cursor = 'pointer';
-    if (props.can_grade) {
-        let dotEl = info.el.getElementsByClassName('fc-event-dot')[0];
-        if (dotEl) {
-            dotEl.style.visibility = 'visible';
-            dotEl.style.backgroundColor = 'white';
-        } else {
-            element.style.backgroundImage =
-                'url("/static/sport/images/categories/sc_trainer.png")';
-            element.style.backgroundPosition = 'right bottom';
-            element.style.backgroundRepeat = 'no-repeat';
-            element.style.backgroundSize = '40%';
-        }
-    }
+	let props = event.extendedProps;
+	element.style.fontSize = '99';
+	element.style.backgroundColor = get_color(event.title);
+	element.style.cursor = 'pointer';
+	if (props.can_grade) {
+		let dotEl = info.el.getElementsByClassName('fc-event-dot')[0];
+		if (dotEl) {
+			dotEl.style.visibility = 'visible';
+			dotEl.style.backgroundColor = 'white';
+		} else {
+			element.style.backgroundImage =
+				'url("/static/sport/images/categories/sc_trainer.png")';
+			element.style.backgroundPosition = 'right bottom';
+			element.style.backgroundRepeat = 'no-repeat';
+			element.style.backgroundSize = '40%';
+		}
+	}
 }
 
 let local_hours_changes = {};
 
 function local_save_hours(e, student_id) {
-    e.parentNode.parentNode.parentNode.className = '';
-    $(e).parent().parent().parent().addClass('table-warning');
-    local_hours_changes[student_id] = parseFloat(e.value);
+	e.parentNode.parentNode.parentNode.className = '';
+	$(e).parent().parent().parent().addClass('table-warning');
+	local_hours_changes[student_id] = parseFloat(e.value);
 }
 
 function show_alert(alert_id, level, text = '') {
-    const alert_elem = document.getElementById(alert_id);
-    alert_elem.className = 'alert alert-' + level;
-    alert_elem.textContent = text;
-    alert_elem.style.visibility = 'visible';
+	const alert_elem = document.getElementById(alert_id);
+	alert_elem.className = 'alert alert-' + level;
+	alert_elem.textContent = text;
+	alert_elem.style.visibility = 'visible';
 }
 
 function hide_alert(alert_id) {
-    const alert_elem = document.getElementById(alert_id);
-    if (alert_elem) {
-        alert_elem.textContent = '';
-        alert_elem.style.visibility = 'hidden';
-    }
+	const alert_elem = document.getElementById(alert_id);
+	if (alert_elem) {
+		alert_elem.textContent = '';
+		alert_elem.style.visibility = 'hidden';
+	}
 }
 
 function parse_local_storage() {
-    let parsed = [];
-    for (let [key, value] of Object.entries(local_hours_changes)) {
-        parsed.push({
-            student_id: key,
-            hours: value,
-        });
-    }
-    return parsed;
+	let parsed = [];
+	for (let [key, value] of Object.entries(local_hours_changes)) {
+		parsed.push({
+			student_id: key,
+			hours: value,
+		});
+	}
+	return parsed;
 }
 
 async function save_hours() {
-    if (Object.keys(local_hours_changes).length === 0) return;
-    const btn = $('#save-hours-btn');
-    btn.prop('disabled', true);
-    const training_id = btn.attr('data-training-id');
+	if (Object.keys(local_hours_changes).length === 0) return;
+	const btn = $('#save-hours-btn');
+	btn.prop('disabled', true);
+	const training_id = btn.attr('data-training-id');
 
-    let hours_valid = true; // Check for validity of hours
-    let invalid_row_count = 0;
-    let first_invalid_row = null;
-    for (const key in local_hours_changes) {
-        if (
-            local_hours_changes[key] < 0 ||
-            local_hours_changes[key] > current_duration_academic_hours
-        ) {
-            hours_valid = false;
+	let hours_valid = true; // Check for validity of hours
+	let invalid_row_count = 0;
+	let first_invalid_row = null;
+	for (const key in local_hours_changes) {
+		if (
+			local_hours_changes[key] < 0 ||
+			local_hours_changes[key] > current_duration_academic_hours
+		) {
+			hours_valid = false;
 
-            const invalid_row_id = 'student_' + key.toString();
-            const invalid_row = document.getElementById(invalid_row_id); // get row
-            if (invalid_row.className.match(/(?:^|\s)table-warning(?!\S)/)) {
-                invalid_row.classList.remove('table-warning');
-                invalid_row.classList.add('table-danger');
-            }
-            if (first_invalid_row == null) {
-                first_invalid_row = invalid_row;
-            }
-            invalid_row_count += 1;
-        }
-    }
-    if (!hours_valid) {
-        first_invalid_row.scrollIntoView();
-        show_alert(
-            'hours-alert',
-            'danger',
-            'Invalid value of hours in ' +
-            invalid_row_count.toString() +
-            ' row(s)'
-        );
-    } else {
-        await sendResults('/api/attendance/mark', {
-            training_id,
-            students_hours: parse_local_storage(),
-        });
+			const invalid_row_id = 'student_' + key.toString();
+			const invalid_row = document.getElementById(invalid_row_id); // get row
+			if (invalid_row.className.match(/(?:^|\s)table-warning(?!\S)/)) {
+				invalid_row.classList.remove('table-warning');
+				invalid_row.classList.add('table-danger');
+			}
+			if (first_invalid_row == null) {
+				first_invalid_row = invalid_row;
+			}
+			invalid_row_count += 1;
+		}
+	}
+	if (!hours_valid) {
+		first_invalid_row.scrollIntoView();
+		show_alert(
+			'hours-alert',
+			'danger',
+			'Invalid value of hours in ' +
+				invalid_row_count.toString() +
+				' row(s)'
+		);
+	} else {
+		await sendResults('/api/attendance/mark', {
+			training_id,
+			students_hours: parse_local_storage(),
+		});
 
-        $('#grading-modal tr').removeClass('table-warning');
-        local_hours_changes = {};
+		$('#grading-modal tr').removeClass('table-warning');
+		local_hours_changes = {};
 
-        hide_alert('hours-alert');
-        $('#grading-modal').modal('hide');
-    }
-    btn.prop('disabled', false);
-    calc_marked_students();
+		hide_alert('hours-alert');
+		$('#grading-modal').modal('hide');
+	}
+	btn.prop('disabled', false);
+	calc_marked_students();
 }
 
-$(document).on('hidden.bs.modal', '#grading-modal', function() {
-    hide_alert('hours-alert');
+$(document).on('hidden.bs.modal', '#grading-modal', function () {
+	hide_alert('hours-alert');
 });
 
 function round(num, decimal_places) {
-    const decimal = Math.pow(10, decimal_places);
-    return Math.round((num + Number.EPSILON) * decimal) / decimal;
+	const decimal = Math.pow(10, decimal_places);
+	return Math.round((num + Number.EPSILON) * decimal) / decimal;
 }
 
 let student_hours_tbody = null;
@@ -254,14 +255,14 @@ let current_duration_academic_hours = 0;
 let students_in_table = {}; // <student_id: jquery selector of a row in the table>
 
 function add_student_row(
-    student_id,
-    full_name,
-    email,
-    med_group,
-    hours,
-    maxHours
+	student_id,
+	full_name,
+	email,
+	med_group,
+	hours,
+	maxHours
 ) {
-    const row = $(`<tr id="student_${student_id}">
+	const row = $(`<tr id="student_${student_id}">
                     <td class="trainer-table-width show-name-in-trainer-table" onclick="show_email_hide_name()">${full_name} 
                     ${
 						med_group === 'Special 1'
@@ -473,9 +474,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	calendar.render();
 });
 
-function parse_student_from_server(data) {
+function parse_student_from_server(data, h = 0) {
 	const [student_id, full_name, email, med_group] = data.split('_');
-	const hours = 0;
+	const hours = h;
 	const student_row = students_in_table[student_id];
 	if (student_row == null) {
 		// check if current student is in the table
@@ -488,6 +489,7 @@ function parse_student_from_server(data) {
 			hours,
 			maxHours
 		); // add if student isn't present
+		local_hours_changes[student_id] = hours;
 	} else {
 		student_row[0].scrollIntoView(); // scroll to the row with student
 		student_row.delay(25).fadeOut().fadeIn().fadeOut().fadeIn();
@@ -666,7 +668,10 @@ function csv_upload(object) {
 				})
 				.then((data) => {
 					if (data.length === 1) {
-						parse_student_from_server(data[0].value);
+						parse_student_from_server(
+							data[0].value,
+							current_duration_academic_hours
+						); // All students from CSV file get current max hours attendance
 					} else if (data.length === 0) {
 						toastr.error(
 							`There is no such student (${record[0]}) or student does not choose its sport`,
@@ -740,4 +745,4 @@ fetch('/api/fitnesstest/result', {
 			});
 		}
 	})
-	.catch(() => { });
+	.catch(() => {});
