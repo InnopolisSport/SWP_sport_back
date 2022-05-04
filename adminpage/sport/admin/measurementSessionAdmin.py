e1fcf59d50dfdc425223304e60bf9d8bea4cd401
Kirill Kuznetsov <k.kuznetsov@innopolis.university>
2022-05-04 12:45:23 +0300

    Add inline result into session

--- adminpage/sport/admin/measurementSessionAdmin.py
+++ adminpage/sport/admin/measurementSessionAdmin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
 from .site import site
-from sport.models import MeasurementSession
+from sport.models import MeasurementSession, MeasurementResult
+from api.crud.crud_semester import get_ongoing_semester
 
+
+class MeasurementResultInline(admin.TabularInline):
+    model = MeasurementResult
+
+    fields = ("measurement", "value")
+    readonly_fields = ("measurement", )
+
+
+    extra = 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'semester':
+            kwargs['initial'] = get_ongoing_semester()
+            return db_field.formfield(**kwargs)
+        return super().formfield_for_foreignkey(
+            db_field, request, **kwargs
+        )
+
+
+class AddMeasurementResultInline(MeasurementResultInline):
+    extra = 4
+    readonly_fields = ()
+
+
+
+    def has_view_or_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return True
 
 @admin.register(MeasurementSession, site=site)
 class MeasurementSession(admin.ModelAdmin):
@@ -11,3 +45,6 @@ class MeasurementSession(admin.ModelAdmin):
         "semester",
         "approved"
     )
+
+    inlines = (MeasurementResultInline, AddMeasurementResultInline)
+
