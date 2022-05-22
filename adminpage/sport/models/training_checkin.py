0680cbac4a0f7f1328ff8d003dd156410d856818
Artem Batalov <a@batalov.me>
2022-05-22 15:57:39 +0300

    SPORT-550: Add attendance field

--- adminpage/sport/models/training_checkin.py
+++ adminpage/sport/models/training_checkin.py
@@ -5,3 +5,5 @@ class TrainingCheckIn(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     student = models.ForeignKey("Student", on_delete=models.CASCADE, related_name='checkins')
     training = models.ForeignKey("Training", on_delete=models.CASCADE, related_name='checkins')
+    attendance = models.OneToOneField('Attendance',
+                                      null=True, blank=True, on_delete=models.SET_NULL, related_name='checkin')
