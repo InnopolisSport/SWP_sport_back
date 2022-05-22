d9a2a68aad9476f388e386ebc4b5ffb2c7691ce8
Artem Batalov <a@batalov.me>
2022-05-23 02:47:59 +0300

    Update settings.py

--- adminpage/adminpage/settings.py
+++ adminpage/adminpage/settings.py
@@ -49,7 +49,7 @@ DATE_FORMAT = "%Y-%m-%d"
 # People with passed checkup are able to upload self-sport
 SELFSPORT_MINIMUM_MEDICAL_GROUP_ID = -1
 
-JS_VERSION = "S22.16.2"
+JS_VERSION = "Sum22.17.2"
 
 SPORT_DEPARTMENT_EMAIL = "sport@innopolis.university"
 STUDENT_AUTH_GROUP_VERBOSE_NAME = "Students"
