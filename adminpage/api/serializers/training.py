81a449b9d00f917b947902640b57ae91ba1af173
Artem Batalov <a@batalov.me>
2022-05-22 17:06:26 +0300

    SPORT-550: Fix training info

--- adminpage/api/serializers/training.py
+++ adminpage/api/serializers/training.py
@@ -1,5 +1,45 @@
 from rest_framework import serializers
 
+from api.serializers.semester import SemesterSerializer
+from sport.models import Training, Group, Trainer
+
+
+class NewTrainerSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='pk')
+    email = serializers.CharField(source='user.email')
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+
+    class Meta:
+        model = Trainer
+        fields = ('id', 'first_name', 'last_name', 'email')
+
+
+class NewGroupSerializer(serializers.ModelSerializer):
+    semester = SemesterSerializer()
+    teachers = NewTrainerSerializer(source='trainers', many=True)
+
+    class Meta:
+        model = Group
+        fields = ('id', 'name', 'description', 'link_name', 'link', 'capacity', 'is_club', 'sport', 'semester', 'teachers')
+
+
+class NewTrainingInfoSerializer(serializers.ModelSerializer):
+    group = NewGroupSerializer()
+    load = serializers.SerializerMethodField()
+
+    def get_load(self, obj: Training) -> int:
+        return obj.checkins.count()
+
+    class Meta:
+        model = Training
+        fields = ('id', 'group', 'start', 'end', 'load')
+
+class NewTrainingInfoStudentSerializer(serializers.Serializer):
+    training = NewTrainingInfoSerializer()
+    can_check_in = serializers.BooleanField()
+    checked_in = serializers.BooleanField()
+
 
 class TrainingInfoSerializer(serializers.Serializer):
     group_id = serializers.IntegerField()
--- adminpage/api/views/training.py
+++ adminpage/api/views/training.py
@@ -4,22 +4,28 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-from api.crud import get_attended_training_info
 from api.permissions import IsStudent
-from api.serializers import TrainingInfoSerializer, NotFoundSerializer
-from sport.models import Training
+from api.serializers import NotFoundSerializer
+from api.serializers.training import NewTrainingInfoSerializer, NewTrainingInfoStudentSerializer
+from sport.models import Training, Student
 
 
 @swagger_auto_schema(
     method="GET",
     responses={
-        status.HTTP_200_OK: TrainingInfoSerializer,
-        status.HTTP_404_NOT_FOUND: NotFoundSerializer,
+        status.HTTP_200_OK: NewTrainingInfoSerializer(),
+        status.HTTP_404_NOT_FOUND: NotFoundSerializer(),
     }
 )
 @api_view(["GET"])
 @permission_classes([IsStudent])
 def training_info(request, training_id, **kwargs):
-    student = request.user  # user.pk == user.student.pk
-    get_object_or_404(Training, pk=training_id)
-    return Response(get_attended_training_info(training_id, student))
+    training = get_object_or_404(Training, pk=training_id)
+    student: Student = request.user.student
+    checked_in = training.checkins.filter(student=student).exists()
+    can_check_in = student.medical_group in training.group.allowed_medical_groups.all()
+    return Response(NewTrainingInfoStudentSerializer({
+        'training': training,
+        'can_check_in': can_check_in,
+        'checked_in': checked_in
+    }).data)
