b1a162cc1b036480f2d439b0b06499f4428dc043
Artem Batalov <a@batalov.me>
2022-05-23 02:28:13 +0300

    HOTFIX: delete category

--- adminpage/sport/urls.py
+++ adminpage/sport/urls.py
@@ -6,7 +6,7 @@ from .views import *
 urlpatterns = [
     path('', login_redirect, name="login"),
     path('profile/', profile_view, name='profile'),
-    path('category/', category_view, name='category'),
+    # path('category/', category_view, name='category'),
     path('fitness_test/', fitness_test_view, name='fitness_test'),
     path('fitness_test/new', fitness_test_session_new_view,
          name='fitness_test_session_new'),
