c603147088c6ef6dc434d55982bb8535c5fa4000
Artem Batalov <a@batalov.me>
2022-08-26 14:59:54 +0300

    Draft models

--- adminpage/training_suggestor/models.py
+++ adminpage/training_suggestor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class PowerZone(models.Model):
+    number = models.IntegerField(null=False, unique=True)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    power_zone = models.ForeignKey(PowerZone, on_delete=models.CASCADE, null=False)
+
+
+class ExerciseType(models.Model):
+    type = models.CharField(max_length=50, null=False)
+    description = models.TextField(null=True, blank=True)
+    exercises = models.ManyToManyField(Exercise, blank=True)
