c5a0e17c4c374f565d448c4fd218d2c5c8177050
Artem Batalov <a@batalov.me>
2022-08-27 03:10:17 +0300

    New models

--- adminpage/training_suggestor/admin/exercise.py
+++ adminpage/training_suggestor/admin/exercise.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
+from import_export.admin import ImportMixin
 
 from training_suggestor.admin.site import site
 from training_suggestor.models import Exercise
 
 
 @admin.register(Exercise, site=site)
-class ExerciseAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'power_zone',
-        'repeat',
-        'set',
-        'rest_interval'
-    )
+class ExerciseAdmin(ImportMixin, admin.ModelAdmin):
+    pass
+    # list_display = (
+    #     'name',
+    #     'power_zone',
+    #     'repeat',
+    #     'set',
+    #     'rest_interval'
+    # )
