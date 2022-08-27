c409511b5941ffb0e1c95c14fbf07eec9a0cd253
Artem Batalov <a@batalov.me>
2022-08-27 03:10:51 +0300

    Exercise import

--- adminpage/training_suggestor/admin/exercise.py
+++ adminpage/training_suggestor/admin/exercise.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
+from import_export import resources
 from import_export.admin import ImportMixin
 
 from training_suggestor.admin.site import site
+from training_suggestor.admin.utils.import_export import CommaFloatWidget, SecondsDurationWidget
 from training_suggestor.models import Exercise
+
+
+class ExerciseResource(resources.ModelResource):
+    WIDGETS_MAP = {
+        'FloatField': CommaFloatWidget,
+        'DurationField': SecondsDurationWidget,
+    }
+
+    class Meta:
+        model = Exercise
+        import_id_fields = ()
+        fields = ('name', 'ratio', 'avg_time')
 
 
 @admin.register(Exercise, site=site)
 class ExerciseAdmin(ImportMixin, admin.ModelAdmin):
-    pass
-    # list_display = (
-    #     'name',
-    #     'power_zone',
-    #     'repeat',
-    #     'set',
-    #     'rest_interval'
-    # )
+    resource_class = ExerciseResource
+    list_display = ('name', 'ratio', 'avg_time')
--- adminpage/training_suggestor/admin/utils/import_export.py
+++ adminpage/training_suggestor/admin/utils/import_export.py
@@ -1,4 +1,6 @@
-from import_export.widgets import NumberWidget
+from datetime import timedelta
+
+from import_export.widgets import NumberWidget, Widget
 
 
 class CommaFloatWidget(NumberWidget):
@@ -12,3 +14,20 @@ class CommaFloatWidget(NumberWidget):
         if ',' in value:
             value = value.replace(',', '.')
         return float(value)
+
+
+class SecondsDurationWidget(Widget):
+    """
+    Widget for converting time duration fields.
+    """
+
+    def clean(self, value, row=None, *args, **kwargs):
+        if not value:
+            return None
+
+        value = value.replace(',', '.')
+
+        try:
+            return timedelta(seconds=float(value))
+        except (ValueError, TypeError):
+            raise ValueError("Enter a valid duration.")
