fa4e4404925571592cae27ab956d1f3f1b00b09a
Artem Batalov <a@batalov.me>
2022-10-15 18:10:01 +0300

    .

--- adminpage/training_suggestor/utils.py
+++ adminpage/training_suggestor/utils.py
@@ -4,6 +4,8 @@ from training_suggestor.models import PollResult
 def recalculate_ratios(result: PollResult):
     """Recalculate the ratios for user according to poll"""
     for e in result.answers.all():
+        print(e)
+        print([a for a in e.question.answers if a['answer'] == e.answer])
         answer_with_influence: dict = [a for a in e.question.answers if a['answer'] == e.answer][0]
         time_ratio = 1 + answer_with_influence["time_ratio_influence"]
         working_load_ratio = 1 + answer_with_influence["working_load_ratio_influence"]
