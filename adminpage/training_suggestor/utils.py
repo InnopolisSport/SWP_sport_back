9106dfcca633218e829f6342301c3e91d6777b1d
Artem Batalov <a@batalov.me>
2022-10-15 18:13:33 +0300

    fix error

--- adminpage/training_suggestor/utils.py
+++ adminpage/training_suggestor/utils.py
@@ -4,11 +4,12 @@ from training_suggestor.models import PollResult
 def recalculate_ratios(result: PollResult):
     """Recalculate the ratios for user according to poll"""
     for e in result.answers.all():
-        print(e)
-        print([a for a in e.question.answers if a['answer'] == e.answer])
-        answer_with_influence: dict = [a for a in e.question.answers if a['answer'] == e.answer][0]
-        time_ratio = 1 + answer_with_influence["time_ratio_influence"]
-        working_load_ratio = 1 + answer_with_influence["working_load_ratio_influence"]
-        result.user.time_ratio *= time_ratio
-        result.user.working_load_ratio *= working_load_ratio
-        result.user.save()
+        try:
+            answer_with_influence: dict = [a for a in e.question.answers if a['answer'] == e.answer][0]
+            time_ratio = 1 + answer_with_influence["time_ratio_influence"]
+            working_load_ratio = 1 + answer_with_influence["working_load_ratio_influence"]
+            result.user.time_ratio *= time_ratio
+            result.user.working_load_ratio *= working_load_ratio
+            result.user.save()
+        except IndexError:
+            pass
