84d5d65b89859d4c9a7baf38ca67c318925fd1f9
Artem Batalov <a@batalov.me>
2023-01-24 01:33:10 +0300

    HOTFIX: accrdit migration

--- adminpage/sport/migrations/0119_group_accredited.py
+++ adminpage/sport/migrations/0119_group_accredited.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='group',
             name='accredited',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=True),
         ),
     ]
