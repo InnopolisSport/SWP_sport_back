43a2c8fea01b484a5ef2197baee93a2deff3123f
Artem Batalov <a@batalov.me>
2023-01-24 15:34:38 +0300

    HOTFIX: Protect check-in

--- adminpage/sport/models/training_checkin.py
+++ adminpage/sport/models/training_checkin.py
@@ -4,7 +4,7 @@ from django.db import models
 class TrainingCheckIn(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     student = models.ForeignKey("Student", on_delete=models.CASCADE, related_name='checkins')
-    training = models.ForeignKey("Training", on_delete=models.CASCADE, related_name='checkins')
+    training = models.ForeignKey("Training", on_delete=models.PROTECT, related_name='checkins')
     attendance = models.OneToOneField('Attendance',
                                       null=True, blank=True, on_delete=models.SET_NULL, related_name='checkin')
 
