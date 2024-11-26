a9bb4e14a06298d4a56de7f1fc70a2f66acae4a8
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-11-26 22:22:58 +0300

    chore: update JS_VERSION to update caches

--- adminpage/adminpage/settings.py
+++ adminpage/adminpage/settings.py
@@ -49,7 +49,7 @@ DATE_FORMAT = "%Y-%m-%d"
 # People with passed checkup are able to upload self-sport
 SELFSPORT_MINIMUM_MEDICAL_GROUP_ID = -1
 
-JS_VERSION = "F24.22.20"
+JS_VERSION = "F24.22.22"
 
 SPORT_DEPARTMENT_EMAIL = "sport@innopolis.university"
 STUDENT_AUTH_GROUP_VERBOSE_NAME = "Students"
